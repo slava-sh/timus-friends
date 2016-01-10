8e29ce10ff4dcd8dd037d84b49a911a43ae4ddf9
Slava Shklyaev <shk.slava@gmail.com>
2016-01-10 17:10:36 +0200

    Add explicit count=25 to ranklist menu link

--- app/scripts.babel/menu.js
+++ app/scripts.babel/menu.js
@@ -1,9 +1,10 @@
 init({}, () => {
   const friendsLink = document.createElement('a');
-  friendsLink.href = '/ranklist.aspx?friends';
+  friendsLink.href = '/ranklist.aspx?friends&count=1000';
   friendsLink.innerText = getMessage('friends_ranklist');
 
   const ranklistLink = document.querySelector('a[href="/ranklist.aspx"]');
+  ranklistLink.href += '?count=25';
   ranklistLink.parentNode.insertBefore(friendsLink, ranklistLink.nextSibling);
   ranklistLink.parentNode.insertBefore(document.createElement('br'), friendsLink);
 });
