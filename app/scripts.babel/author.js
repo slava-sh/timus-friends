62b02f1b2316bcd7b65c45dd2f0450c9fd96011b
Slava Shklyaev <shk.slava@gmail.com>
2016-01-10 18:06:52 +0200

    Tweak author page button

--- app/scripts.babel/author.js
+++ app/scripts.babel/author.js
@@ -1,7 +1,5 @@
 init({ friends: true }, ({ friends }) => {
   const profileId = getQueryVariable('id');
-  const currentlyFollowing = friends.hasOwnProperty(profileId);
-  const button = createButton(currentlyFollowing, profileId);
-  const authorName = document.querySelector('.author_name');
-  authorName.appendChild(button);
+  const button = createButton(friends.hasOwnProperty(profileId), profileId);
+  document.querySelector('.author_name').appendChild(button);
 });
